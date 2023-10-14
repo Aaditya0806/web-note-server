1fcd09ef05ac2da5e709306b2cb8f3422e4c55c4
Aditya <aadimalviyaa@gmail.com>
2023-10-14 21:42:50 +0530

    add url

--- routes/server.js
+++ routes/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 app.use(
-  cors({ origin: "https://notezipperapi.onrender.com", credentials: true })
+  cors({ origin: "https://web-notes-mkyu.onrender.com/", credentials: true })
 );
 
 connectiondb();
