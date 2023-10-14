98b6b29d83b8598efe745203fdb79878760406a8
Aditya <aadimalviyaa@gmail.com>
2023-10-14 22:04:12 +0530

    add cors

--- routes/server.js
+++ routes/server.js
@@ -14,9 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
-app.use(
-  cors({ origin: "https://web-notes-mkyu.onrender.com/", credentials: true })
-);
+app.use(cors())
 
 connectiondb();
 
